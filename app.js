1e7ce2c724b7c209db274691f9ffa46cfe580234
Ravi <samcollins9899@gmail.com>
2022-09-03 21:39:19 +0530

    added files

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const path = require('path')
+const { readFileSync, writeFileSync } = require("fs")
 
-console.log(path.sep)
-
-const filepath = path.join('/content','subfolder', 'test.txt')
-
-console.log(filepath)
-
-const base = path.basename(filepath)
-console.log(base)

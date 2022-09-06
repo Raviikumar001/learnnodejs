2bbc03c0829ec74cf03cfd1cb2c39a7b7762157d
Ravi <samcollins9899@gmail.com>
2022-09-06 13:13:52 +0530

    added http

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-aaaaa
+
+// const http = require('http');
+
+// const server =http.createServer( (req, res)=> {
+//     console.log('request event');
+//     res.end('Hello world');
+// })
+
+// server.listen (5000,  ()=>{
+//     console.log('server running on port 3000');
+// })
+
+const http = require('http');
+
+const server = http.createServer( (req, res )=> {
+    if(req.url === '/')
+    {
+        res.end('Home');
+    }
+    if(req.url === '/about')
+    {
+        for(let i=0; i<1000; i++)
+        {
+            
+        }
+        res.end('About page');
+    }
+    res.end('Error Page');
+})
+
+server.listen(5000, ()=>{
+    console.log('SERver started on port 5000');
+})

60c297cb51a099489f29df43b47985868e514287
Ravi <samcollins9899@gmail.com>
2022-09-28 23:37:27 +0530

    added new files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 
 const { readFileSync } = require('fs');
 const http = require('http');
- 
-const reader = readFileSync('./index.html','utf-8')
+   
+const reader = readFileSync('./navbar-app/index.html');
+const homeStyles= readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 const server = http.createServer( (req,res)=>{
 
     
@@ -20,7 +23,27 @@ const server = http.createServer( (req,res)=>{
         res.write("<h2>About page</h2>");
         res.end(); 
         
-    }else {
+    }else if(url === '/styles.css')
+    {
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    } 
+    else if(url ==='/logo.svg')
+    { 
+        res.writeHead(200, {'content-type': 'image/svg+xml'});
+        res.write(homeImage);
+        res.end();
+
+    }
+    else if(url === '/browser-app.js')
+    {
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic);
+        res.end();
+
+    }else
+    {
         res.writeHead(404,{"content-type" :"text/html"});
         res.write("<h3>Cannot find the page you're looking for</h3>")
         res.end();

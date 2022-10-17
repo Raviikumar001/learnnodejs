12e71fbd5686fb489284194b71a0788cb75cbe56
Ravi <samcollins9899@gmail.com>
2022-10-18 01:59:44 +0530

    added express middleware

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+
 const express = require('express');
 
 const app = express();
 
+const people = require('./routes/auth')
+const login = require('./routes/people');
 
+app.use(express.static('./methods-public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.listen(5000,()=>{
-    console.log('App is listening on 5000');
+app.use('/api/people',people);
+
+app.use('/login', login);
+
+app.listen(5000, ()=>{
+    console.log('App is listening on port 5000');
 })
+
+
+
+
